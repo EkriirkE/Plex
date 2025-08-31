971b9ac7b0c627bc38e78109cf2c76d6db115380
Erik Johnson <github@ekriirke.net>
2025-08-31 17:40:45 +0200

    Update update.sh
    Ensure cd'ing to the script directory so downloads are in one place. e.g. called from cron

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #2022-08-14 Erik Johnson - EkriirkE
+cd "$(dirname "${BASH_SOURCE[0]}")"
 edeb=$(ls -Art plex*.deb | tail -n 1)
 wget -N --content-disposition "https://plex.tv/downloads/latest/5?channel=16&build=linux-aarch64&distro=debian&X-Plex-Token=xxxxxxxxxxxxxxxxxxxx"
 deb=$(ls -Art plex*.deb | tail -n 1)
