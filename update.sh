d1b5301f077e08a60808bbde35751ac9a9e5216a
Erik Johnson <github@ekriirke.net>
2025-08-31 17:28:41 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#2022-08-14 Erik Johnson - EkriirkE
+edeb=$(ls -Art plex*.deb | tail -n 1)
+wget -N --content-disposition "https://plex.tv/downloads/latest/5?channel=16&build=linux-aarch64&distro=debian&X-Plex-Token=xxxxxxxxxxxxxxxxxxxx"
+deb=$(ls -Art plex*.deb | tail -n 1)
+if [ "$edeb" == "$deb" ]; then
+        echo "No new installation."
+        exit 2
+fi
+dpkg -i "$deb"
+
+#Coment out below if you with to retain prior packages.  Otherwise only the most recently installed version is kept.
+find . -maxdepth 1 -name "plex*.deb" ! -name "$deb" -type f -exec echo {} \;
