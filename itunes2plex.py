337a66dd9f58a3ac94dca19ae6ce9a054ddda7e3
Erik Johnson <github@ekriirke.net>
2025-09-28 01:26:54 +0200

    Fix field rollup

--- itunes2plex.py
+++ itunes2plex.py
@@ -36,8 +36,8 @@ PlexLoc="/media/Music/"
 #Plex SQLite executable, if --direct is passed
 SQLite='./"Plex SQLite"'
 
-def eprint(*args,**kwargs):
-	print(*args,**kwargs,file=sys.stderr)
+def eprint(*args,**kwargs): print(*args,**kwargs,file=sys.stderr)
+def low(s): return s.lower() if s else None
 
 direct="--direct" in sys.argv
 if not os.path.exists(dbf): raise FileNotFoundError(dbf)
@@ -73,7 +73,7 @@ with tempfile.NamedTemporaryFile(mode="w+",delete_on_close=False) if direct else
 					FROM media_parts mp
 					INNER JOIN media_items mi ON mi.id=mp.media_item_id
 					INNER JOIN metadata_items md ON md.id=mi.metadata_item_id
-					WHERE LOWER(file)=LOWER(?)""",(f,)).fetchall()
+					WHERE LOWER(file)=?""",(low(f),)).fetchall()
 			if r:
 				q+=[x["guid"] for x in r]
 				eprint("i",end="",flush=True)
@@ -83,7 +83,7 @@ with tempfile.NamedTemporaryFile(mode="w+",delete_on_close=False) if direct else
 					FROM metadata_items ttl
 					INNER JOIN metadata_items art ON art.id=ttl.parent_id
 					INNER JOIN metadata_items alb ON alb.id=art.parent_id
-					WHERE LOWER(ttl.title)=LOWER(?) AND LOWER(art.title)=LOWER(?) AND LOWER(alb.title)=LOWER(?)""",(d.get("Name"),d.get("Artist") or d.get("Genre"),d.get("Album"))).fetchall()
+					WHERE LOWER(ttl.title)=? AND LOWER(art.title)=? AND LOWER(alb.title)=?""",(low(d.get("Name")),low(d.get("Artist") or d.get("Genre")),low(d.get("Album")))).fetchall()
 			if r:
 				q+=[x["guid"] for x in r if x["guid"] not in q]
 				eprint("m",end="",flush=True)
@@ -92,7 +92,7 @@ with tempfile.NamedTemporaryFile(mode="w+",delete_on_close=False) if direct else
 			r=db.execute("""SELECT ttl.guid,art.guid art_guid
 					FROM metadata_items ttl
 					INNER JOIN metadata_items art ON art.id=ttl.parent_id
-					WHERE LOWER(ttl.title)=LOWER(?) AND LOWER(art.title)=LOWER(?)""",(d.get("Name"),d.get("Artist") or d.get("Genre"))).fetchall()
+					WHERE LOWER(ttl.title)=? AND LOWER(art.title)=?""",(low(d.get("Name")),low(d.get("Artist") or d.get("Genre")))).fetchall()
 			if r:
 				q+=[x["guid"] for x in r if x["guid"] not in q]
 				eprint("s",end="",flush=True)
@@ -107,22 +107,26 @@ with tempfile.NamedTemporaryFile(mode="w+",delete_on_close=False) if direct else
 				except: da=None
 				try: dm=time.mktime(time.strptime(d.get("Date Modified"),"%Y-%m-%dT%H:%M:%SZ"))
 				except: dm=None
+				rd=pd or dm
 				pc=int(d.get("Play Count",0))
 				rt=int(d.get("Rating",0))/10
 				if e:=db.execute("SELECT * FROM metadata_item_settings WHERE account_id=? AND guid=?",(a["id"],guid)).fetchone():
 					#Highest viewcount
-					if e["view_count"] and pc and e["view_count"]>pc: pc=e["view_count"]
+					if not pc or (e["view_count"] and pc and e["view_count"]>pc): pc=e["view_count"]
 					#Most recent play date
-					if e["last_viewed_at"] and pd and e["last_viewed_at"]>pd: pd=e["last_viewed_at"]
+					if not pd or (e["last_viewed_at"] and pd and e["last_viewed_at"]>pd): pd=e["last_viewed_at"]
 					#Earliest creation date
-					if e["created_at"] and da and e["created_at"]<da: da=e["created_at"]
+					if not da or (e["created_at"] and da and e["created_at"]<da): da=e["created_at"]
 					#Most recent modified date
-					if e["updated_at"] and dm and e["updated_at"]>dm: dm=e["updated_at"]
-					#Average rating
-					if e["rating"] and rt: rt=(rt+e["rating"])/2
-					q="UPDATE metadata_item_settings SET rating=?,last_viewed_at=?,view_count=?,created_at=?,updated_at=? WHERE account_id=? AND guid=?",(rt,pd,pc,da,dm,a["id"],guid)
+					if not dm or (e["updated_at"] and dm and e["updated_at"]>dm): dm=e["updated_at"]
+					#Most recent rated date
+					if not rd or (e["last_rated_at"] and rd and e["last_rated_at"]>rd): rd=e["last_rated_at"]
+					#Average rating.  elif?
+					if not rt: rt=e["rating"]
+					elif e["rating"] and rt: rt=(rt+e["rating"])/2
+					q="UPDATE metadata_item_settings SET rating=?,last_viewed_at=?,view_count=?,created_at=?,updated_at=?,last_rated_at=? WHERE account_id=? AND guid=?",(rt,pd,pc,da,dm,rd,a["id"],guid)
 				else:
-					q="INSERT INTO metadata_item_settings (account_id,guid,rating,view_count,last_viewed_at,created_at,updated_at) VALUES (?,?,?,?,?,?,?)",(a["id"],guid,rt,pc,pd,da,dm)
+					q="INSERT INTO metadata_item_settings (account_id,guid,rating,view_count,last_viewed_at,created_at,updated_at,last_rated_at) VALUES (?,?,?,?,?,?,?,?)",(a["id"],guid,rt,pc,pd,da,dm,rd)
 					eprint("+",end="",flush=True)
 				#Can't execute directly due to Plex customizations
 				#db.execute(*q)
